0a321357fddb338521c5134129840adfa94a4440
superdev <newestStar1990@gmail.com>
2024-02-20 00:23:50 -0500

    added save-services

---
+++ src/apps/admin/services/saveAuthorUnique.js
@@ -0,0 +1,15 @@
+import request from 'superagent';
+import base from './base';
+
+import getToken from '../utils/getToken';
+
+export default function (item) {
+    const token = getToken();
+
+    return base(
+        request
+            .post('/api/admin/author/unique')
+            .set('Authorization', `Bearer ${token}`)
+            .send(item)
+    );
+}
---
+++ src/apps/admin/services/saveExampleCategory.js
@@ -0,0 +1,15 @@
+import request from 'superagent';
+import base from './base';
+
+import getToken from '../utils/getToken';
+
+export default function (item) {
+    const token = getToken();
+
+    return base(
+        request
+            .post('/api/admin/example-with-categories/category')
+            .set('Authorization', `Bearer ${token}`)
+            .send(item)
+    );
+}
