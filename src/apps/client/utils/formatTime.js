ec7ee9ec568b2f3d8806cc582dd98a2e20cff7ab
Super <newestStar1990@gmail.com>
2024-02-24 11:24:23 -0500

    Revert "added utils"
    This reverts commit 9fdc0c09e88f3617cd142e8f27c36260a0481d34.

--- src/apps/client/utils/formatTime.js
+++
@@ -1,5 +0,0 @@
-export const formatTime = (date, noSeconds) => {
-    const newTime = new Date(date);
-
-    return newTime.toLocaleTimeString([], !noSeconds ? { hour: '2-digit', minute: '2-digit', second: '2-digit' } : { hour: '2-digit', minute: '2-digit' });
-};
--- src/apps/client/utils/sortFeatured.js
+++
@@ -1,5 +0,0 @@
-export default function (arrayOfIds, arrayOfFeatured) {
-    return arrayOfFeatured.sort(function (a, b) {
-        return arrayOfIds.indexOf(a._id) - arrayOfIds.indexOf(b._id);
-    });
-};
